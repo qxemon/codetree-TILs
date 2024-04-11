508fdb4d556a829b299d4a8e62211b16e83fa27a
Now <parkyw0321@gmail.com>
2024-04-11 09:51:55 +0900

    Title: 코드트리 채점기, Time: 629ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240411/코드트리 채점기/codetree-judger.java
+++ 240411/코드트리 채점기/codetree-judger.java
@@ -5,49 +5,54 @@ public class Main {
 
     static int Q, N;
     static int query;
-    static Map<String, History> map; // url 시작 종료시간을 정리할 map
+
+    static PriorityQueue<WaitingQueue> waitingQueue; // waitingQueue
     static Map<String, Integer> waitingQueueList; // waitingQueue리스트 저장할 map (중복 체크)
-    static boolean[] queue; // 채점기 가능 여부
-    static PriorityQueue<WaitingQueue> pq; // waitingQueue
+    static Map<Integer, JudgingQueue> judgingMap;
+    static Map<String, History> history; // url 시작 종료시간을 정리할 map
+    static boolean[] judger; // 채점기 가능 여부
     static Map<Integer, String> process; // 사용중인 채점기와 채점중인 domain 확인 용
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Q = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        map = new HashMap<>();
-        waitingQueueList = new HashMap<>();
-        process = new HashMap<>();
-        pq = new PriorityQueue<>((o1, o2) -> {
+
+        waitingQueue = new PriorityQueue<>((o1, o2) -> {
             if (o1.priority != o2.priority) {
                 return Integer.compare(o1.priority, o2.priority);
             }
             return Integer.compare(o1.inputTime, o2.inputTime);
         });
+        waitingQueueList = new HashMap<>();
+        judgingMap = new HashMap<>();
+        history = new HashMap<>();
+        process = new HashMap<>();
+
 
         for (int q = 0; q < Q; q++) {
             st = new StringTokenizer(br.readLine());
             query = Integer.parseInt(st.nextToken());
             if (query == 100) {
                 N = Integer.parseInt(st.nextToken());
-                queue = new boolean[N+1];
+                judger = new boolean[N + 1];
                 String url = st.nextToken();
                 ready(url);
             } else if (query == 200) {
-            	int time = Integer.parseInt(st.nextToken());
-            	int priority = Integer.parseInt(st.nextToken());
-            	String url = st.nextToken();
-            	
-            	request(time, priority, url);
+                int time = Integer.parseInt(st.nextToken());
+                int priority = Integer.parseInt(st.nextToken());
+                String url = st.nextToken();
+
+                request(time, priority, url);
             } else if (query == 300) {
-            	int time = Integer.parseInt(st.nextToken());
-            	
-            	gradingTry(time);
+                int time = Integer.parseInt(st.nextToken());
+
+                gradingTry(time);
             } else if (query == 400) {
-            	int time = Integer.parseInt(st.nextToken());
-            	int idx = Integer.parseInt(st.nextToken());
-            	end(time, idx);
-            			
+                int time = Integer.parseInt(st.nextToken());
+                int idx = Integer.parseInt(st.nextToken());
+                end(time, idx);
+
             } else if (query == 500) {
                 print();
             }
@@ -61,10 +66,9 @@ public class Main {
         StringTokenizer st = new StringTokenizer(url, "/");
         String domain = st.nextToken();
 
-        pq.add(new WaitingQueue(0, 1, url));
+        waitingQueue.add(new WaitingQueue(0, 1, url));
         waitingQueueList.put(url, 1);
-        map.put(domain, new History(1,0,url,1));
-        
+
 //        System.out.println("1: pq size " + pq.size());
 
     }
@@ -78,8 +82,8 @@ public class Main {
         String domain = st.nextToken();
 
         if (waitingQueueList.getOrDefault(url, 0) == 0) {
-        	
-            pq.add(new WaitingQueue(time, priority, url)); // pq
+
+            waitingQueue.add(new WaitingQueue(time, priority, url)); // pq
             waitingQueueList.put(url, 1); // 동일 url 체크할 map
         }
     }
@@ -88,38 +92,44 @@ public class Main {
     // 300 t
     // t초에 채점 대기 큐에서 채점이 가능한 경우 우선순위가 가장 높은 채점 task 골라 진행
     public static void gradingTry(int t) {
-        if(pq.peek() == null) return;
-    	
-    	for (int i = 1; i < queue.length; i++) {
-            if (!queue[i]) {
+        if (waitingQueue.peek() == null) return;
 
-            	
-                WaitingQueue wq = pq.peek();
-                
+        for (int i = 1; i < judger.length; i++) {
+            if (!judger[i]) {
+
+
+                WaitingQueue wq = waitingQueue.peek();
+
 //                System.out.println(wq.url);
                 StringTokenizer st = new StringTokenizer(wq.url, "/");
                 String domain = st.nextToken();
-                
-                
-                
-                if(map.get(domain) != null) {
-                	History h = map.get(domain);
 
+
+                //이전 사용 기록이 있다면 적절성 여부 파악해야하므로
+                if (history.get(domain) != null) {
+                    History h = history.get(domain);
 //                		System.out.println(h.start+" "+h.end);
-                        if (t > h.start + (3 * (h.end-h.start))){
-                            // 진행 큐로 옮기기
-                        	wq = pq.poll(); // 큐에서 진짜 뺌
-                        	map.put(domain, new History(t, 0,wq.url,i));
-                        	process.put(i, wq.url);
-                            queue[i] = true;
-                            waitingQueueList.remove(wq.url);
-                        }
-                    
+                    if (t > h.start + (3 * (h.end - h.start))) {
+                        // 진행 큐로 옮기기
+                        wq = waitingQueue.poll(); // 큐에서 진짜 뺌
+                        judgingMap.put(i, new JudgingQueue(t, wq.url, i));
+                        process.put(i, wq.url);
+                        judger[i] = true;
+                        waitingQueueList.remove(wq.url);
+                    }
+
+                    break;
+
+                } else if (history.get(domain) == null) {
+
+                    wq = waitingQueue.poll();
+                    judgingMap.put(i, new JudgingQueue(t, wq.url, i));
+                    judger[i] = true;
+                    waitingQueueList.remove(wq.url);
+                    break;
                 }
 
 
-                
-                break;
             }
         }
 
@@ -129,16 +139,16 @@ public class Main {
     // 400 t j_id
     // t초에 j_id번 채점이 종료
     public static void end(int t, int idx) {
-    	// idx 에 있는 domain 찾고 endTime 갱신하기
-    	
-    	if(process.get(idx) != null) {
-    		String url = process.get(idx);
-        	String[] domain = url.split("/");
-        	int start = map.get(domain[0]).start;
-        	map.put(domain[0], new History(start, t, url, idx)); // 종료시간까지 갱신
-        	queue[idx] = false;
-        	waitingQueueList.remove(url);
-    	}
+        // idx 에 있는 domain 찾고 endTime 갱신하기
+        if(judgingMap.get(idx) != null){
+            JudgingQueue jq = judgingMap.get(idx);
+            String url = jq.url;
+            String[] domain = url.split("/");
+            int start = jq.start;
+
+            history.put(domain[0], new History(start, t, url, idx));
+            judger[idx] = false;
+        }
 
     }
 
@@ -146,10 +156,10 @@ public class Main {
     // 500 t
     // t에 채점 대기 큐에 있는 채점 task 수 출력
     public static void print() {
-        System.out.println(pq.size());
+        System.out.println(waitingQueue.size());
     }
 
-    static class JudgingQueue{
+    static class JudgingQueue {
         int start;
         String url;
         int jId;
@@ -161,7 +171,7 @@ public class Main {
         }
     }
 
-    static class History{
+    static class History {
         int start;
         int end;
         String url;
@@ -174,8 +184,6 @@ public class Main {
             this.jId = jId;
         }
     }
-
-
 
 
     static class WaitingQueue implements Comparator<WaitingQueue> {
@@ -197,23 +205,6 @@ public class Main {
             }
             return Integer.compare(o1.inputTime, o2.inputTime);
         }
-    }
-
-
-    static class Time {
-        int start;
-        int end;
-
-        public Time(int start) {
-            this.start = start;
-        }
-
-		public Time(int start, int end) {
-			this.start = start;
-			this.end = end;
-		}
-        
-        
     }
 
 
