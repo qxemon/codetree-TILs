4deeda682a898966950b746b0b402d615da1017b
Now <parkyw0321@gmail.com>
2024-04-21 21:57:28 +0900

    Title: 사각형 채우기 3, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240421/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 2;
+        if(n == 1) {
+            System.out.println(2);
+            return;
+        }
+
+        dp[2] = 7;
+
+        for(int i= 3; i<=n; i++){
+            dp[i] = (dp[i-1] * dp[i-2]) % 1000000007;
+
+        }
+
+        System.out.println(dp[n]);
+
+
+    }
+}
